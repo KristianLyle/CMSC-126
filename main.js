a5a06cea2fac3766ece69aeba1f8522107276b0a
IcyMorales <igmorales1@up.edu.ph>
2023-05-03 22:25:50 +0800

    Added an example of content page and it sub content
    I already updated the css and js and make a link in home page to ethnic page

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 function time_now() {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    const date = new Date();
-    const month = months[date.getMonth()];
-    const day = date.getDate();
-    const year = date.getFullYear();
-
-    const dateString = `${month} ${day}, ${year}`;
-    const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
-    const timeString = date.toLocaleTimeString('en-PH', timeOptions);
-
-    document.getElementById("current-date").textContent = `${dateString}`;
+	document.getElementById("current-date").innerHTML = new Date().toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"});
 }
