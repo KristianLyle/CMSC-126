ef7a322cd41d04ec165e604fa6aea37a7361e9d1
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2023-05-04 23:25:13 +0800

    Added password checker in sign-up page
    Added password checker in sign-up page so if the user used less than 8 characters, it will not be accepted. The same error will occur when the user re-enter a password in the re-enter field that doesn't match the password value inputted on the password field.

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 function time_now() {
 	document.getElementById("current-date").innerHTML = new Date().toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"});
 }
+
+let users = [];
+
+function add_user() {
+    let id = Math.floor(Math.random() * 1000000);
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let repassword = document.getElementById("repassword").value;
+
+    const warning = document.getElementById("warning");
+    const note = document.getElementById("note");
+    if(password.length >= 8){
+        if(password == repassword){
+            let user = {
+                id: id,
+                username: username,
+                password: password,
+              };
+            users.push(user);
+        }
+        else{
+            warning.innerHTML = "Passwords do not match!";
+        }
+    }
+    else{
+        note.innerHTML = "Passwords must be at least 8 characters";
+    }
+}
