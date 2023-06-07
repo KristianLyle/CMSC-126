68dfd521ded8d954b60cb8525710435bc42926d5
KristianLyle <kbsencil1@up.edu.ph>
2023-06-08 02:43:46 +0800

    added content Editor to Pages

--- contentEditor.php
+++ contentEditor.php
@@ -1,26 +1,52 @@
 <?php
 include('serverConn.php');
 
-$sql = "SELECT * FROM content WHERE cont_id = '1'";
+$id = $_POST['cont_id'] ?? '';
+
+echo $id;
+
+$sql = "SELECT * FROM content WHERE cont_id = '$id'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 ?>
 
 <form action="contentEditor.php" method="POST">
+  <input type="hidden" id ="cont_id" name="cont_id" value="<?php echo $id ?>">
   <textarea name="contentText" id="contentText" rows="10" style="height: 300px; width: 1000px"><?php echo isset($row['cont_text']) ? $row['cont_text'] : ''; ?></textarea>
-  <input type="submit" value="Update">
+  <input type="submit" name="update" value="Update">
 </form>
+
+<button type="button" onclick="goBack()">Back</button>
+
+<script>
+   function goBack() {
+        let referrer = document.referrer;
+        if (referrer.endsWith(".html")) {
+            window.location.href = referrer;
+        } else {
+            window.history.go(-2);
+    }
+  }
+</script>
 
 <?php
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $content = $_POST['contentText'] ?? '';
+$id = $_POST['cont_id'] ?? '';
 
-    $updateContent = "UPDATE content SET cont_text = '$content' WHERE cont_id ='1'";
-    $updateResult = $conn->query($updateContent);
+echo $id;
 
-    $conn->close();
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['update'])) {
+      $content = $_POST['contentText'] ?? '';
+      $escapedContent = mysqli_real_escape_string($conn, $content);
+  
+      $updateContent = "UPDATE content SET cont_text = '$escapedContent' WHERE cont_id ='$id'";
+      $updateResult = $conn->query($updateContent);
+  
+      $conn->close();
+    }
+  }
+
 ?>
 
-<button type="button" onclick="location.href='prehistoricPeriod.html'"> Back </button>
+
